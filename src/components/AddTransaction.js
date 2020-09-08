1718c1f3e7cb62bfa7bae7cd397e6773e02f03af
luke-h1 <luke.howsam@yahoo.com>
2020-09-08 19:23:44 +0100

    init commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000),
+      text,
+      amount: parseInt(amount),
+    };
+    addTransaction(newTransaction);
+  };
+
   return (
     <Fragment>
       <h3>Add Transaction</h3>
-      <form id='form'>
+      <form onSubmit={onSubmit}>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
-          <input type='text' id='text' placeholder='Enter text...' />
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Enter text...'
+          />
         </div>
         <div className='form-control'>
           <label html='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type='number' id='amount' placeholder='Enter amount...' />
+          <input
+            type='number'
+            placeholder='Enter amount...'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder='Enter text...'
+          />
         </div>
         <button className='btn'>Add transaction</button>
       </form>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,16 @@
-import React, { Fragment } from 'react';
-
+import React, { useContext, Fragment } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+  console.log(transactions);
   return (
     <Fragment>
       <h3>History</h3>
       <ul className='list'>
-        <li className='minus'>
-          Cash <span>-£400</span>
-          <button className='delete-btn'>X</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </Fragment>
   );
